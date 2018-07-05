dc176fcdc5fa1421ce0413c025ad90e001bfe483
infernalslam <infernal-slam@hotmail.com>
2018-07-05 13:13:42 +0700

    init docker-compose kafka

---
+++ index.js
@@ -0,0 +1,22 @@
+const kafka = require('kafka-node')
+const Consumer = kafka.Consumer
+const client = new kafka.Client('0.0.0.0:9092')
+// const client = new kafka.KafkaClient({kafkaHost: '0.0.0.0:9092'});
+const consumer = new Consumer(client, [{
+  topic: 'test-kafka',
+  offset: 0
+}], {
+  autoCommit: true
+})
+
+consumer.on('message', function (message) {
+  console.log(message)
+})
+
+consumer.on('error', function (err) {
+  console.log('Error:', err)
+})
+
+consumer.on('offsetOutOfRange', function (err) {
+  console.log('offsetOutOfRange:', err)
+})
