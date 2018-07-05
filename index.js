aebf76baa894ca5610d7b739b69129c28fb8bf53
infernalslam <infernal-slam@hotmail.com>
2018-07-05 13:43:06 +0700

    fix code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const kafka = require('kafka-node')
 const Consumer = kafka.Consumer
-const client = new kafka.Client('0.0.0.0:9092')
+const client = new kafka.Client()
 // const client = new kafka.KafkaClient({kafkaHost: '0.0.0.0:9092'});
 const consumer = new Consumer(client, [{
   topic: 'test-kafka',
