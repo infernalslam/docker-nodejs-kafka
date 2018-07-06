cedfc80cb1d33d269ef64defde84b9bf6d426ab0
infernalslam <infernal-slam@hotmail.com>
2018-07-06 12:31:35 +0700

    remove code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const kafka = require('kafka-node')
 const Consumer = kafka.Consumer
 const client = new kafka.Client()
-// const client = new kafka.KafkaClient({kafkaHost: '0.0.0.0:9092'});
 const consumer = new Consumer(client, [{
   topic: 'test-kafka',
   offset: 0
